df4787ae9f7fe9f4ae86bac2eb388de8f5435081
dilane56 <kamgaingfotso@gmail.com>
2025-07-02 17:33:38 +0100

    creation du hooks d'authentification

---
+++ src/hooks/use-auth.tsx
@@ -0,0 +1,98 @@
+"use client"
+import {createContext} from "node:vm";
+import {ReactNode, use, useContext, useEffect, useState} from "react";
+
+interface  User{
+    id: number,
+    email: string,
+    nom: string,
+    prenom : string,
+    role:"ADMIN" | "MEDECIN" | "SECRETAIRE"|"PATIENT",
+    telephone?:string,
+    specialite?: string,
+    sexe?:"HOMME"| "FEMME",
+    addresse?: string,
+
+
+}
+
+interface AuthContextType {
+    user : User| null
+    login: ( email: string, password: string ) => Promise<User>
+    logout: () => void,
+    isLoading: boolean
+}
+
+const  AuthContext = createContext<AuthContextType | undefined> (undefined)
+
+export function  AUthProvider({ children }:{children: ReactNode}){
+    const [user, setUser] = useState<User | null>(null)
+    const [isLoading, setisLoading] = useState(true)
+
+    useEffect(() => {
+        //verifier sil'utilisateur est deja connecté au chargement
+        const  token = localStorage.getItem("auth_token")
+        const userData = localStorage.getItem("user_data")
+
+        if (token && userData){
+            try {
+                const parseUser = JSON.parse(userData)
+                setUser(parseUser)
+                console.log("Utilisateur cnnecté :", parseUser)
+            } catch (error){
+                console.error("Erreur lors du parsn des données utilisateur: ", error)
+                localStorage.removeItem("auth_token")
+                localStorage.removeItem("user_data")
+            }
+        }
+    })
+
+    const apiUrl =""
+
+    const login = async (email: string , password: string ): Promise<User> =>{
+        console.log("Tentative de connextion pour: ", email)
+
+        try{
+            const response = await fetch(`${apiUrl}/login`,{
+                method: "POST",
+                headers: {
+                    "content-Type": "application/json",
+                },
+                body: JSON.stringify({ email, password }),
+            });
+
+            if (!response.ok){
+                throw new Error("Echec de la connexion : identifiants invalides");
+            }
+        }
+
+        const data = await response.json();
+        const {token, user} = data;
+
+        //Stocker le token et les donées utilisateur
+        localStorage.setItem("auth_token", token);
+        localStorage.setItem("user_data", JSON.stringify(user));
+        setUser(user)
+        console.log("connexion réussie:", user);
+        return user
+
+    }
+    const logout = () => {
+        localStorage.removeItem("auth_token")
+        localStorage.removeItem("user_data")
+        setUser(null)
+        console.log("Déconnexion réussie")
+    }
+    return (
+        <AuthContext.Provider value={{user, login, logout, isLoading}}>{children}</AuthContext.Provider>)
+
+
+
+}
+export function useAuth(): AuthContextType {
+    const context = useContext(AuthContext)
+    if(context === undefined){
+        throw new Error("useAuth doit être utilisé dans un AuthProvider")
+    }
+    return context
+}
